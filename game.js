9dc68fdd253d544fc38583dd3e1b2d306fa57239
Jarullah <jarullah479@gmail.com>
2022-04-20 12:46:02 +0500

    Add playRound() and game() functions
    playRound() plays a single round and keeps track of wins and ties
    game() function plays the game 5 times and announces the final
    result

--- game.js
+++ game.js
@@ -1,8 +1,61 @@
-function computerPlay(){
+let playerWins = 0, computerWins = 0, ties = 0;
+
+function computerPlay() {
     let options = ['Rock', 'Paper', 'Scissors'];
     return options[Math.floor(Math.random() * 3)];
 }
 
+function playRound(playerSelection, computerSelection) {
+    let result = 'Invalid input! Try again';
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if(playerSelection == computerSelection){
+        result = `Tie ! ${playerSelection} vs ${computerSelection}`;
+        ties++;
+    }else if(playerSelection == 'rock'){
+        if(computerSelection == 'paper'){
+            result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            computerWins++;
+        }else if(computerSelection == 'scissors'){
+            result = `You Win! ${playerSelection} beats ${computerSelection}`;
+            playerWins++;
+        }
+    } else if (playerSelection == 'paper'){
+        if(computerSelection == 'rock'){
+            result = `You Win! ${playerSelection} beats ${computerSelection}`;
+            playerWins++;
+        } else if (computerSelection == 'scissors'){
+            result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            computerWins++;
+        }
+    } else if (playerSelection == 'scissors'){
+        if(computerSelection == 'rock'){
+            result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            computerWins++;
+        } else if (computerSelection == 'paper'){
+            result = `You Win! ${playerSelection} beats ${computerSelection}`;
+            playerWins++;
+        }
+    }
+
+    return result;
+}
+
+function game() {
+    for(let i = 0; i < 5; i++){
+        let playerChoice = window.prompt("Rock, Paper, or Scissors :");
+        let computerChoice = computerPlay();
+        let result = playRound(playerChoice, computerChoice);
+        window.alert(result);
+        console.log(result);
+    }
+
+    let final_result = `player: ${playerWins} \ncomputer: ${computerWins}\nties: ${ties} \nWinner : ${playerWins > computerWins? 'player': 'computer'}`;
+    window.alert(final_result);
+    console.log(final_result);
+}
 
 
+// Play the game. 5 rounds.
+game();
 
